f4af08cbf02951c09affa4a88c05d44661604aa7
ZakirovaVera <zakirovavk@gmail.com>
2022-10-15 21:21:20 +0500

    Добавлена схема программы

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import phonebook
 
 number = -1
 phonebook.initialization()
-while (number!=0):
+while number!=0:
     number = menu.view()
     if number == 1:
         phonebook.show()
+        print()
     if number == 2:
         phonebook.new_record()
+        print()
     if number == 3:
         phonebook.remove_record()
+        print()
     if number == 4:
         phonebook.save_file()
+        print()
